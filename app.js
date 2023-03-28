1b84f12f48464a36fa778d211bd39effb40dba48
aminnicevic04 <aminnicevic@gmail.com>
2023-03-29 00:02:03 +0200

    Rest Country API

--- app.js
+++ app.js
@@ -1,30 +1,71 @@
-let kartica = document.querySelector(".card");
+const dropDown = document.querySelector(".dropdown-menu");
+const dropOptions = document.querySelector(".drop-options");
+const country = document.querySelector(".country");
+const countries = document.querySelector(".countries");
+const search = document.querySelector(".search");
+const regions = document.querySelectorAll(".regions");
 
-const fetchAll = async () => {
-  const response = await fetch("https://restcountries.com/v3.1/all ");
-  const responseData = await response.json();
-  console.log(responseData);
-  // container.innerHTML = <div><h1>${element.name.common}</h1></div>;
-  return responseData;
-};
-let countries;
+dropDown.addEventListener("click", (e) => {
+  dropOptions.classList.toggle("show-options");
+});
 
-fetchAll().then((d) => {
-  podaciDrzave = d;
-  d.map((element) => {
-    kartica.innerHTML += `
-      <div>
-        <img src="${element.flags.png}" />
-        <div class="innerCard">
-          <h3>
-            <span>Country:</span> <span>${element.name.common}</span>
-          </h3>
-          <h3>
-            <span>Capital city:</span> <span>${element.capital}</span>
-          </h3>
-          <h3>
-            <span>Population:</span> <span>${element.population}</span>
-          </h3>
-      </div>`;
+async function getCountries() {
+  const URL = await fetch("https://restcountries.com/v3.1/all");
+  const res = await URL.json();
+  console.log(res);
+  res.forEach((api) => {
+    showCountry(api);
+  });
+}
+
+getCountries();
+
+function showCountry(data) {
+  const country = document.createElement("div");
+  country.classList.add("country");
+  country.innerHTML = `<div class="country-img">
+    <img src=${data.flags.svg} alt="">
+  </div>
+  <div class="country-details">
+    <h5 class="country-name">${data.name.common}</h5>
+    <p><strong>Population: </strong>${data.population}</p>
+    <p class="region-name"><strong>Region: </strong>${data.region}</p>
+    <p><strong>Capital: </strong>${data.capital}</p>
+  </div>`;
+
+  countries.appendChild(country);
+}
+
+const countryName = document.getElementsByClassName("country-name");
+search.addEventListener("input", (e) => {
+  Array.from(countryName).forEach((country) => {
+    if (country.innerText.toLowerCase().includes(search.value.toLowerCase())) {
+      country.parentElement.parentElement.style.display = "grid";
+    } else {
+      country.parentElement.parentElement.style.display = "none";
+    }
   });
 });
+
+const regionName = document.getElementsByClassName("region-name");
+regions.forEach((region) => {
+  region.addEventListener("click", (e) => {
+    Array.from(regionName).forEach((element) => {
+      if (
+        element.innerText.includes(region.innerText) ||
+        region.innerText === "All"
+      ) {
+        element.parentElement.parentElement.style.display = "grid";
+      } else {
+        element.parentElement.parentElement.style.display = "none";
+      }
+    });
+  });
+});
+
+function output() {
+  console.log(countries);
+}
+
+const allData = document.getElementsByClassName("country");
+document.addEventListener("click", output, true);
